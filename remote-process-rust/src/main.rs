c03ae4b2a1e03449d63953ed7474c739fbd8d119
Rajput, Rajat <Rajat.Rajput@aristocrat.com>
2022-04-28 12:09:13 +0530

    server is done

--- remote-process-rust/src/main.rs
+++ remote-process-rust/src/main.rs
@@ -2,7 +2,12 @@ use communication::{
     responder_server::{Responder, ResponderServer},
     RequestMsg, ResponseMsg,
 };
-use tonic::{Request, Response, Status};
+use local_ip_address::{list_afinet_netifas, Error as LocalIpError};
+use std::net::{AddrParseError, IpAddr, SocketAddr};
+use tonic::{
+    transport::{Error as TonicError, Server},
+    Request, Response, Status,
+};
 
 mod communication {
     tonic::include_proto!("communication");
@@ -21,6 +26,51 @@ impl Responder for RemoteResponder {
 }
 
 #[tokio::main]
-async fn main() {
-    println!("Hello, world!");
+async fn main() -> Result<(), Error> {
+    let addr = format!("{}:50051", get_ip()?).parse::<SocketAddr>()?;
+    let responder = RemoteResponder;
+    Server::builder()
+        .add_service(ResponderServer::new(responder))
+        .serve(addr)
+        .await?;
+    Ok(())
+}
+
+#[derive(Debug)]
+enum Error {
+    OrgIpNotListed,
+    LocalIp(LocalIpError),
+    AddrParser(AddrParseError),
+    Tonic(TonicError),
+}
+
+fn get_ip() -> Result<IpAddr, Error> {
+    list_afinet_netifas()?
+        .iter()
+        .find_map(|v| {
+            if v.1.to_string().contains("172.") {
+                Some(v.1)
+            } else {
+                None
+            }
+        })
+        .ok_or_else(|| Error::OrgIpNotListed)
+}
+
+impl From<LocalIpError> for Error {
+    fn from(e: LocalIpError) -> Self {
+        Error::LocalIp(e)
+    }
+}
+
+impl From<AddrParseError> for Error {
+    fn from(e: AddrParseError) -> Self {
+        Error::AddrParser(e)
+    }
+}
+
+impl From<TonicError> for Error {
+    fn from(e: TonicError) -> Self {
+        Error::Tonic(e)
+    }
 }
