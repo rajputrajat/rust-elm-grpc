3f4aa56ea1c32032e363976436b71bcd7311cba8
Rajput, Rajat <Rajat.Rajput@aristocrat.com>
2022-04-28 13:03:37 +0530

    still the same

--- webapp-companion-wasm/src/lib.rs
+++ webapp-companion-wasm/src/lib.rs
@@ -1,15 +1,16 @@
 tonic::include_proto!("communication");
 
 use grpc_web_client::Client;
+use responder_client::ResponderClient;
 use tonic::Request;
 
 pub async fn send_msg_to_remote() -> Result<(), Error> {
     let client = Client::new("http://127.0.0.1:8080".to_owned());
-    let client = responder_client::ResponderClient::new(client);
+    let client = ResponderClient::new(client);
 
     let resp = client
         .calling_out(Request::new(RequestMsg {
-            please_msg: "how are you, Server?".to_owned(),
+            please_msg: "how are you, Server?".into(),
         }))
         .await?;
     Ok(())
