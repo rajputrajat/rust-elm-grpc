a8ef9b6878e18a3dd0100249486bd2fc01d6fad3
Rajput, Rajat <Rajat.Rajput@aristocrat.com>
2022-04-28 11:56:47 +0530

    progressing ahead

--- remote-process-rust/src/main.rs
+++ remote-process-rust/src/main.rs
@@ -1,7 +1,8 @@
 use communication::{
     responder_server::{Responder, ResponderServer},
-    RequestMsg, RespnseMsg,
+    RequestMsg, ResponseMsg,
 };
+use tonic::{Request, Response, Status};
 
 mod communication {
     tonic::include_proto!("communication");
@@ -9,8 +10,15 @@ mod communication {
 
 struct RemoteResponder;
 
-// #[tonic::async_trait]
-// impl
+#[tonic::async_trait]
+impl Responder for RemoteResponder {
+    async fn calling_out(&self, req: Request<RequestMsg>) -> Result<Response<ResponseMsg>, Status> {
+        println!("request: {req:?}");
+        Ok(Response::new(ResponseMsg {
+            loud_reply: "We are fine. you take care.".to_owned(),
+        }))
+    }
+}
 
 #[tokio::main]
 async fn main() {
