0176db27223526a5fec1d6457a08b9efff4d3a05
Rajput, Rajat <Rajat.Rajput@aristocrat.com>
2024-08-23 19:58:03 +0530

    wip

--- webapp-companion-wasm/src/lib.rs
+++ webapp-companion-wasm/src/lib.rs
@@ -8,11 +8,11 @@ pub async fn send_msg_to_remote() -> Result<(), Error> {
     let client = Client::new("http://127.0.0.1:8080".to_owned());
     let client = ResponderClient::new(client);
 
-    let resp = client
-        .calling_out(Request::new(RequestMsg {
-            please_msg: "how are you, Server?".into(),
-        }))
-        .await?;
+    // let resp = client
+    //     .calling_out(Request::new(RequestMsg {
+    //         please_msg: "how are you, Server?".into(),
+    //     }))
+    //     .await?;
     Ok(())
 }
 
